685c559eaedbe8f922bd4760b1fb169c94001427
Aske21 <veledarevica@yahoo.com>
2024-04-02 00:19:30 +0200

    Milestone #1 - project structure/ design and components / styling

---
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -0,0 +1,19 @@
+# Title
+
+## Description
+
+Describe the purpose of this pull request.
+
+## Changes
+
+List the changes made in this pull request. Include details such as added features, bug fixes, refactored code, etc.
+
+## Checklist
+
+- [ ] I have tested the changes locally.
+- [ ] My code follows the project's coding standards.
+- [ ] My changes do not introduce any new linting warnings or errors.
+
+## Additional Notes
+
+Add any additional notes or context about this pull request.
---
+++ App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {SafeAreaView, Text, useColorScheme} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+function App(): React.JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <Text>hi</Text>
+    </SafeAreaView>
+  );
+}
+
+export default App;
